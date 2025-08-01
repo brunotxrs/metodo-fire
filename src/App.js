3bdebee85bac5e343c74f1b8ae7b324b3f6c122f
brunotxrs <183097017+brunotxrs@users.noreply.github.com>
2025-08-01 13:26:32 -0400

    feat: update elements of project, and update styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import profiler from './img/profile.sarom-metodo-fire.png';
+import profiler from './img/profile-metodo-fire.png';
 
 function App() {
   return (
@@ -7,25 +7,29 @@ function App() {
 
       <div className="box-App">
 
-        <h1>MÉTODO FIRE</h1>
+        <h1>
+          Venda nas<br/> 
+          Redes com<br/> 
+          Estratégia
+        </h1>
 
-        <h2>JUNTA-SE A NÓS HOJE NO MÉTODO FIRE!</h2>
+        <h2>
+          Aprenda a<br/> 
+          aumentar<br/> 
+          suas<br/> 
+          <strong>VENDAS</strong><br/> 
+          com o FIRE
+        </h2>
+
 
         <p>
-          É HORA DE DEIXAR <br/> PARA TRÁS <br/> 
-          UMA VIDA<br/> DE ATRASO E<br/> RENOVAR <br/> 
-          SEU CAMINHO<br/> RUMO AO<br/> SUCESSO
+          Destrave suas <br/> vendas de uma vez <br/> 
+          por todas através<br/> da internet, usando<br/> 
+          suas redes sociais.
         </p>
 
-        <span>
-          OBTENHA  O 
-          MÉTODO FIRE 
-          CLICANDO AQUI!
-        </span>
-
-
         <button>
-          <a href='https://pay.hotmart.com/D101062351S' target='_blank'>MÉTODO FIRE</a>
+          <a href='https://pay.hotmart.com/O101138882Q' target='_blank'>ENTRAR PARA O FIRE</a>
         </button>
 
         <img src={profiler} />
